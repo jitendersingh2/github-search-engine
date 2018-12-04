3d17821a905f5e496e1eb089cf89f222b8891a05
Jitender Singh <jitender.singh2@cognizant.com>
2018-12-04 13:19:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>GITHUB SEARCH ENGINE</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
